6006b07baaf044d08b47a4e941626013fb0bf055
David Zdravkovic <davidzdravkovic21@gmail.com>
2025-08-19 08:54:24 +0200

    Upload current project, remove old files

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "../include/menu.h"
-#include "../include/User.h"
-#include "../include/Account.h"
-#include "../include/personalUser.h"
-
-int main () {
-  mainMenu();
-}
